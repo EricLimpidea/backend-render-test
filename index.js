709f14711fa7bf22ee3623a0be48c6cc67987479
Eric Langrognet <eric.langrognet@limpidea.com>
2025-12-24 20:22:12 +0100

    Update CORS configuration to allow specific origins

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // ✅ Autorise les requêtes venant de n'importe quel site (OK pour test)
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://frontend-vercel-test-erics-projects-8f4c3330.vercel.app",
+      http://localhost:3000,
+      "https://TON-DOMAINE.com",
+    ],
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello depuis Render 👋");
