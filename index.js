9883fd2c6c367209d230ccddb6a65d043d04a3e9
Eric Langrognet <eric.langrognet@limpidea.com>
2025-12-24 20:19:20 +0100

    Enable CORS

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+// ✅ Autorise les requêtes venant de n'importe quel site (OK pour test)
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello depuis Render 👋");
